a33df02c88ac154c45b167b559610d6726524da5
unknown <villarroel24kyle@gmail.com>
2021-06-18 20:46:51 -0400

    added user agent

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from logging import debug
 from flask import Flask
+from flask.globals import request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
