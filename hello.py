63e3c6f95a53d4b083ed69f59d5fb70b5ed25987
unknown <villarroel24kyle@gmail.com>
2021-06-18 21:21:03 -0400

    added redirect method

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from logging import debug
 from flask import Flask
 from flask.globals import request
 from flask.helpers import make_response
+from flask import redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,6 +23,10 @@ def cookie():
     response_one = make_response('<h1>This document carries a cookie!</h1>')
     response_one.set_cookie('answer', '42')
     return response_one
+
+@app.route('/redi')
+def redi():
+    return redirect('http://localhost:5000/user/user_redirected')
 
 if __name__ == '__main__':
     app.run(debug=True)
